eb6520e7bae44d76628f3789e973d9b59b624ada
polyndrom <polyndromdev@gmail.com>
2020-02-22 22:57:33 +1000

    Added enemy

---
+++ core/src/polyndrom/simple_td/enemy/Enemy.java
@@ -0,0 +1,11 @@
+package polyndrom.simple_td.enemy;
+
+import com.badlogic.gdx.math.Vector2;
+
+public abstract class Enemy {
+
+    public Vector2 position;
+
+    public abstract void render();
+
+}

8158c6d59c5541b1d186cccf5551c05c928f00ae
polyndrom <polyndromdev@gmail.com>
2020-03-10 00:49:41 +1000

    Enemy moving, rotating, damage hitting, and some refactoring

--- core/src/polyndrom/simple_td/GameHandler.java
+++ core/src/polyndrom/simple_td/GameHandler.java
@@ -2,22 +2,31 @@ package polyndrom.simple_td;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.TimeUtils;
 import polyndrom.simple_td.enemy.Enemy;
+import polyndrom.simple_td.enemy.TankEnemy;
 import polyndrom.simple_td.tower.Tower;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class GameHandler {
 
-    private List<Tower> towers = new ArrayList<>();
-    private List<Enemy> enemies = new ArrayList<>();
+    private List<Tower> towers = new CopyOnWriteArrayList<>();
+    private List<Enemy> enemies = new CopyOnWriteArrayList<>();
     private Vector2 enemySpawn;
     private Vector2 base;
+    private Level level;
 
-    public GameHandler(Vector2 base, Vector2 enemySpawn) {
-        this.base = base;
-        this.enemySpawn = enemySpawn;
+    public GameHandler(Level level) {
+        System.out.println(Utils.SP2MP(new Vector2(0, 0)));
+        System.out.println(Utils.SP2MP(new Vector2(48, 90)));
+        System.out.println(Utils.SP2MP(new Vector2(48 * 3 + 1, 48 * 5)));
+        this.level = level;
+        this.base = level.getBase();
+        this.enemySpawn = level.getEnemySpawn();
+        List<Vector2> path = PathFinder.find(level, Utils.SP2MP(enemySpawn), Utils.SP2MP(base));
+        enemies.add(new TankEnemy(path));
     }
 
     public void render(SpriteBatch batch) {
@@ -25,21 +34,38 @@ public class GameHandler {
             tower.render(batch);
         }
         for (Enemy enemy: enemies) {
-            enemy.render();
+            enemy.render(batch);
         }
     }
 
+    long t = TimeUtils.millis() - 1500;
+
     public void handle(float delta) {
+        if (TimeUtils.timeSinceMillis(t) >= 1500) {
+            enemies.add(new TankEnemy(PathFinder.find(level, Utils.SP2MP(enemySpawn), Utils.SP2MP(base))));
+            t = TimeUtils.millis();
+        }
+        for (Tower tower: towers) {
+            tower.handle(delta);
+        }
         for (Tower tower: towers) {
             for (Enemy enemy: enemies) {
-                Vector2 enemyPosition = enemy.position;
-                if (tower.radius().contains(enemyPosition) ||
-                    tower.radius().contains(enemyPosition.cpy().add(Constants.BLOCK_WIDTH, 0)) ||
-                    tower.radius().contains(enemyPosition.cpy().add(0, Constants.BLOCK_HEIGHT)) ||
-                    tower.radius().contains(enemyPosition.cpy().add(Constants.BLOCK_WIDTH, Constants.BLOCK_HEIGHT))) {
-                    tower.attack(enemy);
+                if (tower.isEnemyInRadius(enemy)) {
+                    if (!tower.isAttacked(enemy)) {
+                        tower.startAttack(enemy);
+                    }
+                } else {
+                    if (tower.isAttacked(enemy)) {
+                        tower.stopAttack(enemy);
+                    }
                 }
             }
+        }
+        for (Enemy enemy: enemies) {
+            if (!enemy.isAlive()) {
+                enemies.remove(enemy);
+            }
+            enemy.handle();
         }
     }
 
--- core/src/polyndrom/simple_td/enemy/Enemy.java
+++ core/src/polyndrom/simple_td/enemy/Enemy.java
@@ -1,11 +1,90 @@
 package polyndrom.simple_td.enemy;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import polyndrom.simple_td.Constants;
+import polyndrom.simple_td.Utils;
+
+import java.util.List;
 
 public abstract class Enemy {
 
-    public Vector2 position;
+    protected List<Vector2> path;
+    protected int currentPoint = 0;
+    protected Vector2 position;
+    protected int hp;
+    protected int rotation = 0;
 
-    public abstract void render();
+    // Enemy properties
+    public abstract int speed();
+    public abstract int damage();
 
+    protected abstract void doMove();
+
+    public abstract void render(SpriteBatch batch);
+
+    public void hit(int damage) {
+        hp -= damage;
+    }
+
+    public boolean isAlive() {
+        return hp > 0;
+    }
+
+    public void handle() {
+        int byX = 0;
+        int byY = 0;
+        int prevX = (int) (currentPoint - 1 < 0 ? path.get(currentPoint).x - 48 : path.get(currentPoint - 1).x);
+        int prevY = (int) (currentPoint - 1 < 0 ? path.get(currentPoint).y: path.get(currentPoint - 1).y);
+        int currentX = (int) path.get(currentPoint).x;
+        int currentY = (int) path.get(currentPoint).y;
+        int nextX = (int) path.get(currentPoint + 1).x;
+        int nextY = (int) path.get(currentPoint + 1).y;
+        if (currentX == nextX) {
+            if (currentY < nextY)  byY = 1;
+            else byY = -1;
+        }
+        if (currentY == nextY) {
+            if (currentX < nextX) byX = 1;
+            else byX = -1;
+        }
+        if (prevY == currentY && currentX == nextX) {
+            if (byY == 1) rotation = 90;
+            else rotation = 270;
+        }
+        if (prevX == currentX && currentY == nextY) {
+            if (byX == 1) rotation = 0;
+            else rotation = 180;
+        }
+        position.x += speed() * Gdx.graphics.getDeltaTime() * byX;
+        position.y += speed() * Gdx.graphics.getDeltaTime() * byY;
+        int x = (int) path.get(currentPoint + 1).x * 48;
+        int y = (int) path.get(currentPoint + 1).y * 48;
+        Rectangle nextRect = new Rectangle(x + 23, y + 23, 2, 2);
+        if (nextRect.contains(position.cpy().add(24, 24))) {
+
+            currentPoint++;
+        }
+        doMove();
+    }
+
+    public Enemy(List<Vector2> path) {
+        this.path = path;
+        position = Utils.MP2SP(path.get(0));
+    }
+
+    public Rectangle hitBox() {
+        return new Rectangle(position.x + Constants.BLOCK_WIDTH / 4f, position.y + Constants.BLOCK_HEIGHT / 4f,
+                Constants.BLOCK_WIDTH / 2f, Constants.BLOCK_HEIGHT / 2f);
+    }
+
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    public int getHp() {
+        return hp;
+    }
 }
